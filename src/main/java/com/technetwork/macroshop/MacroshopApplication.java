9cec55c962dd25efb22df4f1afed5abbe583009a
ciprian2398 <ciprian2398@gmailcom>
2020-10-12 18:24:43 +0300

    Initial commit

---
+++ src/main/java/com/technetwork/macroshop/MacroshopApplication.java
@@ -0,0 +1,13 @@
+package com.technetwork.macroshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroshopApplication.class, args);
+	}
+
+}
